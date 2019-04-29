73d144918dfc00b41f784321ec8b775da9589cca
Liam Stewart <lstewart2397@gmail.com>
2019-04-28 20:26:20 -0400

    ability to click to copy IP added

--- popup/app.js
+++ popup/app.js
@@ -8,12 +8,19 @@ browser.tabs.query({
     //hack to get just domain
     let parser = document.createElement('a');
     parser.href = url;
-
     let urlFetch = 'http://ip-api.com/json/' + parser.hostname; //url to fetch
+    let copyField = document.querySelector('#copyField');
+
+    function copyToClipboard() {
+        copyField.select();
+        document.execCommand("copy");
+    }
+
     fetch(urlFetch)
       .then(res => res.json())
       .then((output) => {
-        document.body.innerHTML = output.query;
+        copyField.value = output.query;
+        document.addEventListener('click',copyToClipboard,false);
       })
       .catch(err => {
         document.body.innerHTML = ':(';
