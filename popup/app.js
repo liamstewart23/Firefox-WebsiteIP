d5d23306cf4d7b490e35e53219c989e7ebc3b500
Liam Stewart <lstewart2397@gmail.com>
2021-06-15 19:29:06 -0400

    ...

--- popup/app.js
+++ popup/app.js
@@ -4,22 +4,42 @@ browser.tabs.query({
     active: true
 })
     .then((tabs) => {
+
+        // Get Tab URL
+
         let parser = document.createElement('a');
         parser.href = tabs[0].url;
-        let resolving = browser.dns.resolve(parser.hostname);
-        resolving.then((record) =>{
+
+        // IPv4 Lookup
+
+        let resolvingIPv4 = browser.dns.resolve(parser.hostname, [
+            'disable_ipv6'
+        ]);
+
+        resolvingIPv4.then((record) => {
             let IPv4 = document.querySelector('#IPv4');
-            let IPv6 = document.querySelector('#IPv6');
-            IPv4.value = record.addresses[1];
-            IPv6.value = record.addresses[0];
-            IPv4.addEventListener('click',() => {
+            IPv4.value = record.addresses[0];
+            IPv4.addEventListener('click', () => {
                 IPv4.select();
                 document.execCommand('copy');
-            },false);
-            IPv6.addEventListener('click',() => {
-                IPv6.select();
-                document.execCommand('copy');
-            },false);
+            }, false);
 
         });
+
+        // IPv6 Lookup
+
+        let resolvingIPv6 = browser.dns.resolve(parser.hostname, [
+            'disable_ipv4'
+        ]);
+
+        resolvingIPv6.then((record) => {
+            let IPv6 = document.querySelector('#IPv6');
+            IPv6.value = record.addresses[0];
+            IPv6.addEventListener('click', () => {
+                IPv6.select();
+                document.execCommand('copy');
+            }, false);
+
+        });
+
     });
